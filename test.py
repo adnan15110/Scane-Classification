b2ab6757c47697d3e8f92ac0589447a969ec03d8
Adnan <khan.adnanalam@gmail.com>
2015-07-16 20:23:53 -0500

    dataset for classification created

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
 import numpy as np
+from data_reader import DataReader
+import os,shutil
+from dataset_creator import trainTestandValidationDataCreator
 
-print 'test'
+# shutil.copyfile(os.path.join(os.getcwd(),'labels.txt'),\
+# 	os.path.join(os.getcwd(),'labels-bak.txt'))
+
+t_class = trainTestandValidationDataCreator()
+t_class.createDatasetForClassificationProblem()
+
+
+# d = DataReader('dataset.csv')
+# d.csvReader()
